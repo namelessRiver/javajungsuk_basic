c56497d67a05076944e99cb8d07eb4b9406b7f96
namelessRiver <river.nameless@gmail.com>
2022-02-20 18:37:30 +0900

    Ch2_2

---
+++ ch02/src/Ex2_2.java
@@ -0,0 +1,20 @@
+
+public class Ex2_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year = 0, age = 14;
+		year = age + 2000;
+		// year = 14 + 2000
+		// year = 2014
+		
+		age = age + 1;
+		// age = 14 + 1;
+		// age = 15;
+		
+		System.out.println(year);
+		System.out.println(age);
+
+	}
+
+}
