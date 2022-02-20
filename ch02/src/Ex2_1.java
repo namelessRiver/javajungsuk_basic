e5e5999c2bc75d102994dd8fba3870e7eb0ca826
namelessRiver <river.nameless@gmail.com>
2022-02-20 18:24:15 +0900

    ch2_1

---
+++ ch02/src/Ex2_1.java
@@ -0,0 +1,18 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// Çà´ÜÀ§ º¹»ç
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("5+3");
+		System.out.println(5+3); // µ¡¼À
+		System.out.println(5-3); // »¬¼À
+		System.out.println(5*3); // °ö¼À
+		System.out.println(5/3); // ³ª´°¼À
+		System.out.println(5/3.0); // ³ª´°¼À
+	}
+
+}
